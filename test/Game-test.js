a1ada907cc0c4759362bac7eb12c9975918683e8
Steven Mancine <stevenrmancine@gmail.com>
2021-02-02 23:04:37 -0700

    Add methods to Game class

--- test/Game-test.js
+++ test/Game-test.js
@@ -7,31 +7,30 @@ const Deck = require('../src/Deck');
 
 describe('Game', function() {
 
-  it.skip('should be a function', () => {
+  it('should be a function', () => {
     expect(Game).to.be.a('function');
   });
 
-  it.skip('it should instantiate a new Game', () => {
+  it('it should instantiate a new Game', () => {
     const game = new Game();
     expect(game).to.be.an.instanceOf(Game);
   });
 
-  it.skip('should have a start method', () => {
+  it('should have a start method', () => {
     const game = new Game();
     expect(game.start).to.be.a('function');
   });
 
-  it.skip('should create Cards', () => {
+  it('should create Cards', () => {
     const game = new Game();
-    expect(game.createCards()).to.be.an.instanceOf(Card);
+    game.start()
+    expect(game.currentRound.deck.cards[0]).to.be.an.instanceOf(Card);
   });
 
-  it.skip('should make a new Deck with Cards', () => {
+  it('should make a new Deck with Cards', () => {
     const game = new Game();
-    game.createCards();
-    game.buildDeck();
-    expect(game.deck).to.be.an.instanceOf(Deck)
-    expect(game.deck.cards).to.deep.equal(game.createCards())
+    game.start();
+    expect(game.currentRound.deck).to.be.an.instanceOf(Deck)
   });
 
 });
